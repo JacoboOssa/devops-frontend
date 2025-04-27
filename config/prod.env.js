f71863face0eef7acb8fb402b127034066fb535a
Jacobo Ossa <jaco2419@gmail.com>
2025-04-27 11:01:15 -0500

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  AUTH_API_ADDRESS: '"https://api-testing-ca.azure-api.net"',
-  TODOS_API_ADDRESS: '"https://test-todos-api-ca.greensand-b550d21a.eastus.azurecontainerapps.io"'
+  BASE_API_ADDRESS: '"https://api-testing-ca.azure-api.net"',
 }
 
